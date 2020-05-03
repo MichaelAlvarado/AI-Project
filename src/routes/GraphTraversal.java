068092da23702ee0d488ed9a1488a469b6290673
Michael Alvarado <michael.alvarado1@upr.edu>
2020-05-03 00:31:43 -0400

    added DepthFirstSearch traversal

---
+++ src/routes/GraphTraversal.java
@@ -0,0 +1,84 @@
+package routes;
+
+import java.util.LinkedList;
+
+import collection.Graph;
+import collection.Node;
+
+public class GraphTraversal {
+
+	/**
+	 * Breadth First Search Traversal Print Graph
+	 * @author Michael J. Alvarado
+	 * @date May 3, 2020
+	 * @param graph
+	 * @param initialPosition - index of the Node to start the print of Graph
+	 * @throws IndexOutOfBoundsException - - if the index is out of range(index < 0 || index >= size())
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void BFSPrint(Graph graph, int initialPosition) 
+	{
+		//Initialize the visited and queue list
+		boolean visited[] = new boolean[graph.size()]; 
+		LinkedList<Node> queue = new LinkedList<Node>(); 
+
+		// Mark the current node as visited and enqueue it 
+		visited[initialPosition]=true; 
+		queue.add(graph.getNode(initialPosition)); 
+		
+		//BTS
+		while (queue.size() != 0) 
+		{ 
+			// Dequeue a Node from queue and print it 
+			Node node = queue.poll(); 
+			visited[graph.indexOf(node)] = true;
+			System.out.println(node.get().toString()+" ");  
+
+			LinkedList<Node> childrens = node.getNodeChildrens();
+			
+			//Add the childrens not visited or already in queue
+			for (Node nodeChildren : childrens) {
+				if(!visited[graph.indexOf(nodeChildren)] && queue.indexOf(nodeChildren) < 0)
+					queue.add(nodeChildren);	
+			}	
+		} 
+	}
+	
+	/**
+	 * Depth First Search Traversal Print Graph
+	 * @author Michael J. Alvarado
+	 * @date May 3, 2020
+	 * @param graph
+	 * @param initialPosition - index of the Node to start the print of Graph
+	 * @throws IndexOutOfBoundsException - - if the index is out of range(index < 0 || index >= size())
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void DFSPrint(Graph graph, int initialPosition) 
+	{
+		//Initialize the visited and queue list
+		boolean visited[] = new boolean[graph.size()]; 
+		LinkedList<Node> queue = new LinkedList<Node>(); 
+
+		// Mark the current node as visited and enqueue it 
+		visited[initialPosition]=true; 
+		queue.add(graph.getNode(initialPosition)); 
+		
+		//BTS
+		while (queue.size() != 0) 
+		{ 
+			// Dequeue a Node from queue and print it 
+			Node node = queue.pollLast(); 
+			visited[graph.indexOf(node)] = true;
+			System.out.println(node.get().toString()+" ");  
+
+			LinkedList<Node> childrens = node.getNodeChildrens();
+			
+			//Add the childrens not visited or already in queue
+			for (Node nodeChildren : childrens) {
+				if(!visited[graph.indexOf(nodeChildren)] && queue.indexOf(nodeChildren) < 0)
+					queue.add(nodeChildren);	
+			}	
+		} 
+	}
+	
+}
--- src/testers/mainTesting.java
+++ src/testers/mainTesting.java
@@ -1,7 +1,7 @@
 package testers;
 
 import collection.Graph;
-import routes.BreadthFirstSearch;
+import routes.GraphTraversal;
 
 public class mainTesting {
 
@@ -15,35 +15,38 @@ public class mainTesting {
 		graph.addNode(6);
 		graph.addNode(7);
 
-		System.out.println(graph.addEdge(0, 1, 3));
-		System.out.println(graph.addEdge(0, 1, 5));
-		System.out.println(graph.addEdge(0, 3, 2));
-		System.out.println(graph.addEdge(0, 3, 7));
-		System.out.println(graph.addEdge(0, 5, 4));
-		System.out.println(graph.addEdge(0, 4, 6));
-		System.out.println(graph.addEdge(0, 6, 7));
+		graph.addEdge(0, 1, 3);
+		graph.addEdge(0, 1, 5);
+		graph.addEdge(0, 3, 2);
+		graph.addEdge(0, 3, 7);
+		graph.addEdge(0, 5, 4);
+		graph.addEdge(0, 4, 6);
+		graph.addEdge(0, 6, 7);
 
-		BreadthFirstSearch.BFS(graph, 0);
+		System.out.println("breadth first print:");
+		GraphTraversal.BFSPrint(graph, 0);
 
-//		BreadthFirstSearch.BFS(graph, 0);
-//		
-//		Graph<Integer> graph2 = new Graph<Integer>();
-//		graph.addNode(1);
-//		graph.addNode(2);
-//		graph.addNode(3);
-//		graph.addNode(4);
-//		graph.addNode(5);
-//		graph.addNode(6);
-//		graph.addNode(7);
-//
-//		System.out.println(graph.addEdge(0, 1, 2));
-//		System.out.println(graph.addEdge(0, 1, 3));
-//		System.out.println(graph.addEdge(0, 2, 4));
-//		System.out.println(graph.addEdge(0, 2, 5));
-//		System.out.println(graph.addEdge(0, 3, 6));
-//		System.out.println(graph.addEdge(0, 3, 7));
-//		
-//		BreadthFirstSearch.BFS(graph2, 0);
+		System.out.println("\ndepth first print:");
+		GraphTraversal.DFSPrint(graph, 0);
+		//		BreadthFirstSearch.BFS(graph, 0);
+		//		
+		//		Graph<Integer> graph2 = new Graph<Integer>();
+		//		graph.addNode(1);
+		//		graph.addNode(2);
+		//		graph.addNode(3);
+		//		graph.addNode(4);
+		//		graph.addNode(5);
+		//		graph.addNode(6);
+		//		graph.addNode(7);
+		//
+		//		System.out.println(graph.addEdge(0, 1, 2));
+		//		System.out.println(graph.addEdge(0, 1, 3));
+		//		System.out.println(graph.addEdge(0, 2, 4));
+		//		System.out.println(graph.addEdge(0, 2, 5));
+		//		System.out.println(graph.addEdge(0, 3, 6));
+		//		System.out.println(graph.addEdge(0, 3, 7));
+		//		
+		//		BreadthFirstSearch.BFS(graph2, 0);
 	}
 
 }
