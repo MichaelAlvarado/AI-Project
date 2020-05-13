fff5686a3f476f38a1179727602a5276803c0a9d
Michael Alvarado <michael.alvarado1@upr.edu>
2020-05-13 15:18:56 -0400

    Simulated Annealing Refactors and fixes

--- src/routes/SimulatedAnnealing.java
+++ src/routes/SimulatedAnnealing.java
@@ -11,7 +11,11 @@ public class SimulatedAnnealing {
 	
 	//Cooling Parameters
 	protected static double max_Temp =100;
+	protected static double min_Temp = 0.01;
+	//For linear Cooling
 	protected static double cooling_rate = 0.5;
+	//For percentenge Cooling
+	protected static double alpha = 0.99; //the temperature decrease (1-alpha) percent each iteration
 	//Local Variable of use
 	private static Random rand = new Random();
 
@@ -23,16 +27,16 @@ public class SimulatedAnnealing {
 	 * @param end
 	 * @return the goal Node with path on its parents (If used with reconstructPath method it will have a list of nodes making its path)
 	 */
+	@SuppressWarnings("rawtypes")
 	public static Node simulatedAnnealingPath(Node start, Node end) {
 		//temparatures starts a its highest
 		double t = max_Temp;
-		Node s = randomPath(start, end); //Optimize Path
+		Node s = randomPath(start, end, null); 
 		//Uses temperature cooling
-		while (t > 0) {
+		while (t > min_Temp) {
 			//Takes random Path
-			Node mutate = randomPath(start, end);
+			Node mutate = mutate(start, end);
 			double delta_e = s.getValue() - mutate.getValue();
-//			System.out.println(mutate.getValue()+" - " +s.getValue()+" = " +delta_e);
 			//Change paths to most effective
 			if(delta_e>0) {
 				s = reconstructPath(start, mutate).getLast();
@@ -49,30 +53,55 @@ public class SimulatedAnnealing {
 	}
 
 	/**
+	 * Temperature Schedule to use in Simulated Annealing
+	 * @author Michael J. Alvarado
+	 * @date May 13, 2020
+	 * @param temp
+	 * @return
+	 */
+	private static double temperatureSchedule(double temp) {
+		return AlphaTemperatureSchedule(temp);
+	}
+	
+	/**
 	 * Linear Cooling Temperature 
 	 * @author Michael J. Alvarado
 	 * @date May 13, 2020
 	 * @return
 	 */
-	private static double temperatureSchedule(double temp) {
+	private static double linearTemperatureSchedule(double temp) {
 		return temp-(cooling_rate);
+	}
+	
+	/**
+	 * Cooling Temperature by a (1-alpha) percent rate
+	 * @author Michael J. Alvarado
+	 * @date May 13, 2020
+	 * @return returns the 
+	 */
+	private static double AlphaTemperatureSchedule(double temp) {
+		return temp *= alpha;
 	}
 
 	/**
-	 * Used for mutating a already existing path to create a new (Not Delevop Yet)
+	 * Used for mutating a already existing path to create a new
+	 * It takes a random Node from a already existing path and goes another path
 	 * @author Michael J. Alvarado
 	 * @date May 13, 2020
 	 * @param start
 	 * @param end
-	 * @return
+	 * @return the goal Node with path on its parents
 	 */
+	@SuppressWarnings("rawtypes")
 	private static Node mutate(Node start, Node end) {
 		LinkedList<Node> path = GraphTraversal.reconstructPath(start, end);
-		for (Node node : path) {
-			System.out.println(node.get().toString());
+		int nodeIndex = rand.nextInt(path.size()); //random Node to mutate from
+		HashSet<Node> visited = new HashSet<Node>();
+		for(int i = 0; i < nodeIndex; i++) {
+			visited.add(path.get(i));
 		}
-		Node randomMutation = path.get(rand.nextInt(path.size()-2)+1); //take a random node to mutate
-		return randomPath(randomMutation, end);
+//		start = path.get(rand.nextInt(path.size()-2)+1); //take a random node to mutate
+		return randomPath(path.get(nodeIndex), end, visited);
 	}
 
 	/**
@@ -81,11 +110,13 @@ public class SimulatedAnnealing {
 	 * @date May 13, 2020
 	 * @param start - node to start the search
 	 * @param end - goal Node
+	 * @param visited - Send a HashSet of already visited Node so the Algorithm wont travel through them again, if null it will assume no Nodes has been visited
 	 * @return the goal Node with path on its parents
 	 */
 	@SuppressWarnings("rawtypes")
-	private static Node randomPath(Node start, Node end) {
-		HashSet<Node> visited = new HashSet<Node>();
+	private static Node randomPath(Node start, Node end, HashSet<Node> visited) {
+		if(visited == null)
+			visited = new HashSet<Node>();
 		while(!start.equals(end)) {
 			visited.add(start);
 			Edge edge = (Edge) start.getEdges().get(rand.nextInt(start.getNodeChildrens().size()));
@@ -108,7 +139,7 @@ public class SimulatedAnnealing {
 
 	
 	/**
-	 * 
+	 * Testing Purposes
 	 * @author Michael J. Alvarado
 	 * @date May 13, 2020
 	 * @param start
