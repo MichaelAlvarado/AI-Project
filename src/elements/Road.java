f261782762afde1a5832e9f7e9c9e20922873608
Ramphis Lopez <ramphis.lopez@upr.edu>
2020-05-14 02:16:04 -0400

    Road update, commit test

--- src/elements/Road.java
+++ src/elements/Road.java
@@ -1,15 +1,20 @@
 package elements;
 
+import graph.Node;
+
 public class Road {
 	
-	double distance;
+	Node from;
+	Node to;
 	int speedLimit;
 	double traffic;
 	
-	public Road(double distance, int speedLimit) {
-		this.distance = distance;
+	public Road(Node from, Node to, int speedLimit) {
+		this.from = from;
+		this.to = to;
 		this.speedLimit = speedLimit;
 	}
+	
 	private double TravelTime(double distance, int speedLimit, double traffic) {
 		double time = distance/speedLimit;
 		//traffic affects travel-time, is given by a percentage of extra-time
@@ -20,13 +25,20 @@ public class Road {
 		//output time will be in terms of hours
 		return effTime;
 	}
-	private double getDistance() {
-		return this.distance;
+	
+	public static double getDistance(Node from, Node to) {
+		double dX = (from.getX() - to.getX());
+		double dY = (from.getY() - to.getY());
+		
+		return Math.sqrt(dX * dX + dY * dY);
 	}
+	
 	private int speedLimit() {
 		return this.speedLimit;
 	}
+	
 	private double getTraffic() {
 		return this.traffic;
 	}
+	
 }
--- src/routes/Astar.java
+++ src/routes/Astar.java
@@ -1,16 +1,58 @@
 package routes;
 
+import java.util.AbstractMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import elements.Road;
+import graph.Edge;
 import graph.Graph;
 import graph.Node;
 
 public class Astar {
+	
+	// total cost
+	static double f;
+	
+	static Queue<Entry<Node, Double>> frontier = new PriorityQueue<Entry<Node, Double>>();
+	//path
+	static Queue<Node> explored = new LinkedList<Node>();
+	
+	
 
 	/**
 	 * A* route path finding
 	 * @author 
 	 * @date May 3, 2020
 	 */
-	public static void ARoute(Graph graph, Node from, Node to){
+	public static void ARoute(Graph graph, Node from, Node goal){
+		Node current = from;
+		LinkedList<Edge> L = current.getEdges();
+		
+		for (Edge e : L) {
+			f = e.getValue() + calculateH(e.getNode(), goal);
+			Map.Entry<Node,Double> entry = new AbstractMap.SimpleEntry<Node, Double>(e.getNode(), f);
+			frontier.add(entry);
+		}
+		
+		while(current != goal){
+			explored.add(frontier.peek().getKey()); 
+			ARoute(graph, frontier.poll().getKey() ,goal);
+			
+		}
 		
 	}
+	
+	
+	
+	public static double calculateH(Node from, Node to){
+		return Road.getDistance(from,to);
+	}
+	
+	
+
+	
 }
