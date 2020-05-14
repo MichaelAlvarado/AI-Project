2ac67f4b801fd1d8dd0bca67ae4588fc629a88bd
Michael Alvarado <michael.alvarado1@upr.edu>
2020-05-14 16:39:47 -0400

    added Puerto Rico map Tester

--- src/elements/Road.java
+++ src/elements/Road.java
@@ -4,32 +4,85 @@ import graph.Node;
 
 public class Road {
 	
-	Node<City> from;
-	Node<City> to;
+	City city1;
+	City city2;
 	
+	double distance;
 	int speedLimit;
 	double traffic;
 	
-	public Road(Node<City> from, Node<City>  to, int speedLimit) {
-		this.from = from;
-		this.to = to;
+	public Road(City from, City  to, double distance, int speedLimit, double traffic) {
+		this.city1 = from;
+		this.city2 = to;
+		this.distance = distance;
 		this.speedLimit = speedLimit;
+		this.traffic = traffic;
 	}
 	
-	public static double TravelTime(double distance, int speedLimit, double traffic) {
-		double time = distance/speedLimit;
+	public City getCity1() {
+		return city1;
+	}
+
+	public void setCity1(City city1) {
+		this.city1 = city1;
+	}
+
+	public City getCity2() {
+		return city2;
+	}
+
+	public void setCity2(City city2) {
+		this.city2 = city2;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public int getSpeedLimit() {
+		return speedLimit;
+	}
+
+	public void setSpeedLimit(int speedLimit) {
+		this.speedLimit = speedLimit;
+	}
+
+	public double getTraffic() {
+		return traffic;
+	}
+
+	public void setTraffic(double traffic) {
+		this.traffic = traffic;
+	}
+
+	public double TravelTime() {
+		double time = minimumTravelTime();
 		//traffic affects travel-time, is given by a percentage of extra-time
 		//traffic depends on the current time it takes
 		//the effective time will be equal to the base time, plus the extra time given by traffic
 		//the effective time will be equal to time, when traffic is 0
-		double effTime = time + time*traffic;
+		time += time*traffic;
 		//output time will be in terms of hours
-		return effTime;
+		return time;
 	}
 	
-	public static double getDistance(Node<City>  from, Node<City>  to) {
-		double dX = (from.get().getX() - to.get().getX());
-		double dY = (from.get().getY() - to.get().getY());
+	/**
+	 * Returns the time to travel this road with no traffic
+	 * @author Michael J. Alvarado
+	 * @date May 14, 2020
+	 * @return
+	 */
+	public double minimumTravelTime() {
+		return distance/speedLimit;
+	}
+	
+	public static double getDistance(City  from, City  to) {
+		double dX = (from.getX() - to.getX());
+		double dY = (from.getY() - to.getY());
 		
 		return Math.sqrt(dX * dX + dY * dY);
 	}
--- src/testers/BigMapTest.java
+++ src/testers/BigMapTest.java
@@ -38,20 +38,109 @@ public class BigMapTest {
 	City lajas = new City("Lajas",0,0);
 	City sanGerman = new City("San German",0,0);
 	City mayaguez = new City("Mayaguez",0,0);
-	City Aguadilla = new City("Aguadilla",0,0);
+	City aguadilla = new City("Aguadilla",0,0);
 
-	@Before
-	public void setUp() throws Exception {
+	private void initNodes() {
 		graph.addNode(cayey);
 		graph.addNode(salinas);
 		graph.addNode(santaIsabel);
 		graph.addNode(barranquitas);
-
-		graph.addEdge(1, cayey, barranquitas); //Cayey to Barranquitas
-		graph.addEdge(1, salinas, cayey); //Salinas to Cayey
-		graph.addEdge(2, salinas, santaIsabel); //Salinas to Santa Isabel
-		graph.addEdge(1, santaIsabel, barranquitas); // Santa Isabel to Barranquita
+		graph.addNode(ponce);
+		graph.addNode(villalba);
+		graph.addNode(caguas);
+		graph.addNode(arroyo);
+		graph.addNode(humacao);
+		graph.addNode(fajardo);
+		graph.addNode(canovana);
+		graph.addNode(sanJuan);
+		graph.addNode(vegaBaja);
+		graph.addNode(manati);
+		graph.addNode(arecibo);
+		graph.addNode(lares);
+		graph.addNode(adjunta);
+		graph.addNode(yauco);
+		graph.addNode(lajas);
+		graph.addNode(sanGerman);
+		graph.addNode(mayaguez);
+		graph.addNode(aguadilla);
+	}
+	
+	private void initRoad() {
+		LinkedList<Road> roads = new LinkedList<Road>();
+		Road a0 = new Road(ponce, santaIsabel, 4, 1, 0);
+		Road a1 = new Road(salinas, santaIsabel, 2, 1, 0);
+		Road a2 = new Road(salinas, cayey, 4, 1, 0);
+		Road a3 = new Road(salinas, arroyo, 4, 1, 0);
+		Road a4 = new Road(arroyo, humacao, 4, 1, 0);
+		Road a5 = new Road(humacao, fajardo, 4, 1, 0);
+		Road a6 = new Road(humacao, caguas, 4, 1, 0);
+		Road a7 = new Road(canovana, fajardo, 4, 1, 0);
+		Road a8 = new Road(canovana, caguas, 4, 1, 0);
+		Road a9 = new Road(sanJuan, canovana, 4, 1, 0);
+		Road a10 = new Road(sanJuan, vegaBaja, 4, 1, 0);
+		Road a11 = new Road(sanJuan, caguas, 4, 1, 0);
+		Road a12 = new Road(cayey, caguas, 4, 1, 0);
+		Road a13 = new Road(cayey, barranquitas, 4, 1, 0);
+		Road a14 = new Road(barranquitas, santaIsabel, 4, 1, 0);
+		Road a15 = new Road(barranquitas, villalba, 4, 1, 0);
+		Road a16 = new Road(ponce, villalba, 4, 1, 0);
+		Road a17 = new Road(ponce, adjunta, 4, 1, 0);
+		Road a18 = new Road(ponce, yauco, 4, 1, 0);		
+		Road a19 = new Road(yauco, sanGerman, 4, 1, 0);
+		Road a20 = new Road(yauco, lajas, 4, 1, 0);
+		Road a21 = new Road(lajas, sanGerman, 4, 1, 0);
+		Road a22 = new Road(sanGerman, mayaguez, 4, 1, 0);
+		Road a23 = new Road(mayaguez, aguadilla, 4, 1, 0);
+		Road a24 = new Road(mayaguez, lares, 4, 1, 0);
+		Road a25 = new Road(lares, adjunta, 4, 1, 0);
+		Road a26 = new Road(lares, arecibo, 4, 1, 0);
+		Road a27 = new Road(aguadilla, arecibo, 4, 1, 0);
+		Road a28 = new Road(arecibo, manati, 4, 1, 0);
+		Road a29 = new Road(manati, barranquitas, 4, 1, 0);
+		Road a30 = new Road(manati, vegaBaja, 4, 1, 0);
+		Road a31 = new Road(vegaBaja, barranquitas, 4, 1, 0);
+		roads.add(a0);
+		roads.add(a1);
+		roads.add(a2);
+		roads.add(a3);
+		roads.add(a4);
+		roads.add(a5);
+		roads.add(a6);
+		roads.add(a7);
+		roads.add(a8);
+		roads.add(a9);
+		roads.add(a10);
+		roads.add(a11);
+		roads.add(a12);
+		roads.add(a13);
+		roads.add(a14);
+		roads.add(a15);
+		roads.add(a16);
+		roads.add(a17);
+		roads.add(a18);
+		roads.add(a19);
+		roads.add(a20);
+		roads.add(a21);
+		roads.add(a22);
+		roads.add(a23);
+		roads.add(a24);
+		roads.add(a25);
+		roads.add(a26);
+		roads.add(a27);
+		roads.add(a28);
+		roads.add(a29);
+		roads.add(a30);
+		roads.add(a31);
+		for(Road road: roads) {
+			graph.addEdge(road.TravelTime(), road.getCity1(), road.getCity2());
+		}
 		
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		initNodes();
+		initRoad();
 	}
 
 	@Test
@@ -60,14 +149,17 @@ public class BigMapTest {
 		LinkedList<Node> result = GraphTraversal.reconstructPath(e);
 		LinkedList<City> expected = new LinkedList<City>();
 		expected.add(salinas);
-		expected.add(cayey);
+		expected.add(santaIsabel);
 		expected.add(barranquitas);
 
 		assertTrue("Result is not the same size as expected", result.size()==expected.size());
 		for (int i = 0; i < result.size(); i++) {
+			System.out.println(expected.get(i).getName());
+			System.out.println(((City)result.get(i).get()).getName());
+
 			assertTrue("Not the optimize solution", result.get(i).get().equals(expected.get(i)));
 		}
-		assertTrue("Not the right value of the Path", e.getValue()==2);
+		assertTrue("Not the right value of the Path", e.getValue()==6);
 	}
 
 }
