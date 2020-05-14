63d326aaaf2cf63e35b1f96f125935a1e74d38b0
Michael Alvarado <michael.alvarado1@upr.edu>
2020-05-14 19:05:25 -0400

    added Puerto Rico Cities x and y and added method to calculate distance.

--- src/elements/City.java
+++ src/elements/City.java
@@ -1,5 +1,7 @@
 package elements;
 
+import java.text.DecimalFormat;
+
 public class City {
 	
 	String name;
@@ -38,7 +40,29 @@ public class City {
 	public void setY(double y) {
 		this.y = y;
 	}
+	
+	/**
+	 * This method Calculate the distance from this to city in Kilometers using google maps pin locations
+	 * @date May 14, 2020
+	 * @param city
+	 * @return
+	 */
+	public double CalculationByDistance(City city) {
+        int Radius=6371;//radius of earth in Km         
+        double lat1 = this.x;
+        double lat2 = city.x;
+        double lon1 = this.y;
+        double lon2 = city.y;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLon = Math.toRadians(lon2-lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult= Radius*c;
 
+        return valueResult;
+     }
 	
 	
 	
